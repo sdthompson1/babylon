6299fca24d14a0a651634a1796ace82d267220fe
sdthompson1 <stephen@solarflare.org.uk>
2026-06-23 14:32:57 +0100

    Move graph stuff and dependency-algorithm to separate "graph" folder

--- isabelle/bab_dependency/BabDependency.thy
+++ isabelle/bab_dependency/BabDependency.thy
@@ -1,5 +1,5 @@
 theory BabDependency
-  imports Main "HOL-Library.Char_ord" "HOL-Library.List_Lexorder" "HOL-Library.FSet" "HOL-Library.Finite_Map" "HOL-Library.Multiset" "../bab/Location" "../bab/BabSyntax" "Dependency"
+  imports Main "HOL-Library.Char_ord" "HOL-Library.List_Lexorder" "HOL-Library.FSet" "HOL-Library.Finite_Map" "HOL-Library.Multiset" "../bab/Location" "../bab/BabSyntax" "../graph/Dependency"
 begin
 
 (* Babylon-specific dependency errors *)
