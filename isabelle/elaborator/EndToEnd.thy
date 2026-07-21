8e977293640fbf586e71a07e7268831450548519
sdthompson1 <stephen@solarflare.org.uk>
2026-07-21 12:01:09 +0100

    isabelle: Add a small comment to EndToEnd.thy explaining why hypothesis `mk` is needed.

--- isabelle/elaborator/EndToEnd.thy
+++ isabelle/elaborator/EndToEnd.thy
@@ -15,6 +15,14 @@ begin
    The proof is pure composition: elab_program_well_typed gives
    core_module_well_typed prog, elab_program_closed gives
    core_module_closed prog, and make_interp_state_matches_env does the rest. *)
+
+(* Note: the hypothesis `mk` (that `make_interp_state` succeeds) is needed only to rule
+   out the case where not all of the required extern functions have been provided. If the
+   first three hypotheses below are true, and additionally the domain of `externs` covers
+   everything needed by the program, then it could be proved that `make_interp_state`
+   will definitely succeed in this case (but we haven't actually formalized this
+   as a theorem). *)
+
 theorem compile_program_interp_state:
   assumes cp: "elab_program modules = Inr ps"
       and link: "whole_program_link ps = Inr prog"
